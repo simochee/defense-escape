8cfa8157eb2e160c4de30fcaa3d7478d8b2ad4be
Ryoya Tamura <lollipop@simochee.net>
2025-03-10 19:47:12 +0900

    Initial commit

---
+++ wxt.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "wxt";
+
+export default defineConfig({
+	manifest: {
+		name: "wxt-template",
+		description: "about this extension",
+	},
+	modules: ["@wxt-dev/auto-icons"],
+});
